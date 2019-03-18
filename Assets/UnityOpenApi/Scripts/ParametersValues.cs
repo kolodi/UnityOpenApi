9309e036c81fa8ab393600c644397d940917e0c6
Michele Kolodi <mykolodiy@gmail.com>
2019-03-18 11:36:59 +0100

    Custom property drawer for ParameterValues

--- Assets/UnityOpenApi/Scripts/ParametersValues.cs
+++ Assets/UnityOpenApi/Scripts/ParametersValues.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace UnityOpenApi
 {
     [Serializable]
     public class ParameterValue
     {
+        [HideInInspector]
         public OAParameter parameter;
         public string value;
         public bool HasValue
@@ -15,4 +20,19 @@ namespace UnityOpenApi
             }
         }
     }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ParameterValue))]
+    public class ParameterValueEditor : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            //OAParameter parameter = property.FindPropertyRelative("parameter");
+            var parNameRect = new Rect(position.x, position.y, EditorGUIUtility.labelWidth, position.height);
+            var parValueRect = new Rect(position.x + EditorGUIUtility.labelWidth, position.y, position.width - (EditorGUIUtility.labelWidth), position.height);
+            EditorGUI.LabelField(parNameRect, property.FindPropertyRelative("parameter.Name").stringValue);
+            EditorGUI.PropertyField(parValueRect, property.FindPropertyRelative("value"), GUIContent.none);
+        }
+    }
+#endif
 }
